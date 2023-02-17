fd18b8cbf87286b168baa5faf0824c8ea7a5e9fb
Shahmeer <shahmeerasim1999@gmail.com>
2023-02-17 14:23:46 +0500

    added the slider

--- main.js
+++ main.js
@@ -4,3 +4,29 @@ const pages = document.getElementsByClassName('pages')[0];
 toggleButton.addEventListener('click', () => {
   pages.classList.toggle('active');
 });
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
